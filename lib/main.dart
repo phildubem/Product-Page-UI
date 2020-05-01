226c71a000ce3cfd19a50f35d212b7d7a45f9a35
Phil Dubem <phildubem@gmail.com>
2020-05-01 15:21:01 +0100

    UI Changes

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,8 @@ class _HomePageState extends State<HomePage> {
            new Container(
              height: 150,
              width: 390,
-             padding: EdgeInsets.only(left: 15.0, top: 28.0, right: 200.0, bottom: 28.0),
+             margin: EdgeInsets.only(right: 18.0, left: 13.0),
+             padding: EdgeInsets.only(left: 15.0, top: 28.0, right: 20.0, bottom: 28.0),
              decoration: BoxDecoration(
                color: Color(
                  0xFFB97BA0,
@@ -106,8 +107,9 @@ class _HomePageState extends State<HomePage> {
                scrollDirection: Axis.horizontal,
                children: <Widget>[
                  Container(
-                   width: 130.0,
-                   margin: EdgeInsets.only(right: 18.0),
+                   padding: EdgeInsets.only(top: 118.0),
+                   width: 135.0,
+                   margin: EdgeInsets.only(right: 20.0),
                    decoration: BoxDecoration(
                      color: Color(0xFFD28D92),
                      borderRadius: BorderRadius.circular(15),
@@ -115,40 +117,98 @@ class _HomePageState extends State<HomePage> {
                    child: Column (
                      crossAxisAlignment: CrossAxisAlignment.center,
                      children: <Widget>[
-                       Text (""),
+                       Column (
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: <Widget>[
+                           Text ("Rotari Pink", style: TextStyle (fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white)),
+                           Text (r"$ " + "11.80", style: TextStyle (color: Colors.white)),
+                         ],
+                       ),
                      ],
                    ),
                  ),
                  Container(
-                   width: 130.0,
-                   margin: EdgeInsets.only(right: 18.0),
+                   padding: EdgeInsets.only(top: 118.0, right: 40.0),
+                   width: 135.0,
+                   margin: EdgeInsets.only(right: 20.0),
                    decoration: BoxDecoration(
                      color: Color(0xFFF3B732),
                      borderRadius: BorderRadius.circular(15),
                    ),
+                   child: Column (
+                     crossAxisAlignment: CrossAxisAlignment.center,
+                     children: <Widget>[
+                       Column (
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: <Widget>[
+                           Text ("Nebla", style: TextStyle (fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white)),
+                           Text (r"$ " + "13.09", style: TextStyle (color: Colors.white)),
+                         ],
+                       ),
+                     ],
+                   ),
                  ),
                  Container(
-                   width: 130.0,
-                   margin: EdgeInsets.only(right: 18.0),
+                   padding: EdgeInsets.only(top: 118.0),
+                   width: 135.0,
+                   margin: EdgeInsets.only(right: 20.0),
                    decoration: BoxDecoration(
                      color: Color(0xFF8CB8A9),
                      borderRadius: BorderRadius.circular(15),
                    ),
+                   child: Column (
+                     crossAxisAlignment: CrossAxisAlignment.center,
+                     children: <Widget>[
+                       Column (
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: <Widget>[
+                           Text ("Nemb Gree", style: TextStyle (fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white)),
+                           Text (r"$ " + "15.00", style: TextStyle (color: Colors.white)),
+                         ],
+                       ),
+                     ],
+                   ),
                  ),
                  Container(
-                   width: 130.0,
-                   margin: EdgeInsets.only(right: 18.0),
+                   padding: EdgeInsets.only(top: 118.0),
+                   width: 135.0,
+                   margin: EdgeInsets.only(right: 20.0),
                    decoration: BoxDecoration(
                      color: Color(0xFF8D3DD0),
                      borderRadius: BorderRadius.circular(15),
                    ),
+                   child: Column (
+                     crossAxisAlignment: CrossAxisAlignment.center,
+                     children: <Widget>[
+                       Column (
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: <Widget>[
+                           Text ("Rotari Purple", style: TextStyle (fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white)),
+                           Text (r"$ " + "12.90", style: TextStyle (color: Colors.white)),
+                         ],
+                       ),
+                     ],
+                   ),
                  ),
                  Container(
-                   width: 130.0,
-                   margin: EdgeInsets.only(right: 5.0),
+                   padding: EdgeInsets.only(top: 118.0),
+                   width: 135.0,
+                   margin: EdgeInsets.only(right: 15.0),
                    decoration: BoxDecoration(
                      color: Color(0xFF04575D),
                      borderRadius: BorderRadius.circular(15),
+                   ),
+                   child: Column (
+                     crossAxisAlignment: CrossAxisAlignment.center,
+                     children: <Widget>[
+                       Column (
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: <Widget>[
+                           Text ("Syrup", style: TextStyle (fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white)),
+                           Text (r"$ " + "21.50", style: TextStyle (color: Colors.white)),
+                         ],
+                       ),
+                     ],
                    ),
                  ),
                ],
